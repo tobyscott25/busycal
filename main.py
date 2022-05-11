dc1837117bae7483278914ccb7d937a4bfa2efb7
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-05-11 16:49:34 +1000

    Create privacy option to show or hide original name of the external event that is being added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ics import Calendar, Event
 import requests
 
 new_cal_name = "busycal_export"
+privacy = False
 
 ext_ics_url = ""
 ext_cal = Calendar(requests.get(ext_ics_url).text)
@@ -18,7 +19,12 @@ new_cal.events.add(example_event)
 
 # Add each event from the set of external calendar events to the new one
 for event in ext_cal.events:
+
+	if privacy == True:
+		event.name = "Busy as a bee!"
+
 	new_cal.events.add(event)
+
 
 with open('%s.ics' % new_cal_name, 'w') as my_file:
     my_file.writelines(new_cal)
