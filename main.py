3768338190f9036bfd6c54251552b8f8cf4a6ac5
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-05-11 17:12:50 +1000

    Now takes an array of .ics URLs to combine

--- main.py
+++ main.py
@@ -2,29 +2,35 @@ from ics import Calendar, Event
 import requests
 
 new_cal_name = "busycal_export"
-privacy = False
+privacy = True
 
-ext_ics_url = ""
-ext_cal = Calendar(requests.get(ext_ics_url).text)
 
 new_cal = Calendar()
 new_cal.creator = new_cal_name # Defines PRODID
 
 # Example Event to add to the Calendar
 example_event = Event()
-example_event.name = "Family Dinner Example Event"
+example_event.name = "Family Dinner (Example Event)"
 example_event.begin = '2023-01-01 00:00:00'
 new_cal.events.add(example_event)
 
 
-# Add each event from the set of external calendar events to the new one
-for event in ext_cal.events:
+# Array of .ics and .ical URLS to include in the export
+ext_ics_urls = []
 
-	if privacy == True:
-		event.name = "Busy as a bee!"
+for ics_url in ext_ics_urls:
+	
+	# Add each event from the set of external calendar events to the new one
+	ext_cal = Calendar(requests.get(ics_url).text)
+	for ext_event in ext_cal.events:
 
-	new_cal.events.add(event)
+		if privacy == True:
+			ext_event.name = "Busy as a bee!"
+
+		new_cal.events.add(ext_event)
 
 
-with open('%s.ics' % new_cal_name, 'w') as my_file:
-    my_file.writelines(new_cal)
+
+
+with open('%s.ics' % new_cal_name, 'w') as export_file:
+    export_file.writelines(new_cal)
