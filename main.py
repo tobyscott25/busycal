418b594a62b79eddb540d917fcf20bc1c5396665
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-05-12 15:49:21 +1000

    Add controlled HTTPException error handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, abort
+from werkzeug.exceptions import HTTPException
 import json
 
 # Create an instance of the class
@@ -8,9 +9,33 @@ app = Flask(__name__)
 @app.route('/<string:name>/')
 def hello(name):
 
+	abort(404, description="Resource not found")
+
 	out = "Hello " + name
-	# Return response in JSON format with status code 200
 	return jsonify(out), 200
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(error):
+	# Start with the correct headers and status code from the error
+	response = error.get_response()
+	# Replace the body with JSON
+	response.data = json.dumps({
+		"code": error.code,
+		"name": error.name,
+		# "description": error.description,
+	})
+	# Set the content type header to idicate JSON
+	response.content_type = "application/json"
+	return response
+
+# For specific errors
+# @app.errorhandler(405)
+# def handle_error(error):
+# 	return {
+# 		"code": 405,
+# 		"name": "Method Not Allowed"
+# 	}, 405
 
 
 # Ensures the Flask app runs only when executed in the main file and not when imported in some other file
