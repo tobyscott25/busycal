9bb06bd655ee34d48c027bab089585a912f91702
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-10-08 16:55:40 +1100

    Move to port 3000

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -12,7 +12,7 @@ func main() {
 	router.GET("/hello-world", helloWorld)
 	router.GET("/hello-world/:id", helloWorldWithID)
 
-	router.Run("localhost:8080")
+	router.Run("localhost:3000")
 }
 
 func helloWorld(c *gin.Context) {
