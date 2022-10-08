a3598623b9339059335ab496e5e99b8e6e0410c1
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-10-08 16:53:23 +1100

    Write Hello World code

---
+++ go-rest-api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello-world", helloWorld)
+	router.GET("/hello-world/:id", helloWorldWithID)
+
+	router.Run("localhost:8080")
+}
+
+func helloWorld(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Hello World!")
+}
+
+func helloWorldWithID(c *gin.Context) {
+	// id := c.Param("id")
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"error": "World not found"})
+}
